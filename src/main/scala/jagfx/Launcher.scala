d4fcce23e437a4cc72da87cec7600c64831cc4f3
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 01:09:49 +0200

    feat: DEBUG level in scribe

--- src/main/scala/jagfx/Launcher.scala
+++ src/main/scala/jagfx/Launcher.scala
@@ -2,4 +2,8 @@ package jagfx
 
 object Launcher:
   def main(args: Array[String]): Unit =
+    scribe.Logger.root
+      .clearHandlers()
+      .withHandler(minimumLevel = Some(scribe.Level.Debug))
+      .replace()
     JagFX.main(args)
