b5744333d00d4a2ae9dcd220bd7a035a52bd6956
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 08:33:17 +0200

    fix import for controller relocation

--- src/main/scala/jagfx/JagFX.scala
+++ src/main/scala/jagfx/JagFX.scala
@@ -4,7 +4,7 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
-import jagfx.ui.MainController
+import jagfx.ui.controller.MainController
 
 val (minWidth, minHeight) = (800, 520)
 
