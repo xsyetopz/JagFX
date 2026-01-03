e3a8ec6f49333a904cc34b64bdbb55b15cdd2d7b
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 07:27:52 +0200

    add base traits

---
+++ src/main/scala/jagfx/ui/viewmodel/IViewModel.scala
@@ -0,0 +1,17 @@
+package jagfx.ui.viewmodel
+
+/** Base trait for ViewModels with change listener support. */
+trait IViewModel:
+  private var listeners: List[() => Unit] = Nil
+
+  /** Register callback to be notified when this `ViewModel` changes. */
+  def addChangeListener(cb: () => Unit): Unit =
+    listeners = cb :: listeners
+    registerPropertyListeners(cb)
+
+  /** Override to wire up property-specific listeners. */
+  protected def registerPropertyListeners(cb: () => Unit): Unit = ()
+
+  /** Notify all registered listeners of change. */
+  protected def notifyListeners(): Unit =
+    listeners.foreach(_())
