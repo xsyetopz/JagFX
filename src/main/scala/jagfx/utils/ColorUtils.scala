2427f5a6cc767a125c88c5f39301c48ca8505774
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 08:30:26 +0200

    remove unused colours

--- src/main/scala/jagfx/utils/ColorUtils.scala
+++ src/main/scala/jagfx/utils/ColorUtils.scala
@@ -3,21 +3,15 @@ package jagfx.utils
 /** Colors synced with `_variables.scss`. */
 object ColorUtils:
   // Backgrounds (from SCSS)
-  val BgRoot: Int = 0xff121212
-  val BgCell: Int = 0xff202020
-  val BgCellSel: Int = 0xff252525
   val BgBlack: Int = 0xff000000
 
   // Borders
   val BorderDim: Int = 0xff333333
-  val BorderAccent: Int = 0xff33bbee
 
   // Text
   val White: Int = 0xfff0f0f0
-  val TextDim: Int = 0xff888888
 
   // Accent
-  val Accent: Int = 0xff33bbee // $bg-fill, $border-accent
   val Graph: Int = 0xffee7733 // $bg-graph (envelope curves)
 
   // Filter
