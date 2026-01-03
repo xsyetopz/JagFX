f05de9b424a875a1166a9025dcca80354e4b327a
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-04 01:51:39 +0200

    rework positioning of items

--- src/main/scala/jagfx/ui/controller/footer/FooterController.scala
+++ src/main/scala/jagfx/ui/controller/footer/FooterController.scala
@@ -4,16 +4,20 @@ import javafx.scene.layout._
 import jagfx.ui.viewmodel.SynthViewModel
 import jagfx.ui.controller.IController
 import jagfx.ui.BindingManager
+import javafx.scene.control.Label
 
 /** Footer controller containing tones, harmonics, reverb, and mode panels. */
-class FooterController(viewModel: SynthViewModel) extends IController[HBox]:
-  protected val view = HBox()
-  view.getStyleClass.add("footer")
+class FooterController(viewModel: SynthViewModel) extends IController[VBox]:
+  private val content = HBox()
+  content.getStyleClass.add("footer")
+
   private val reverbBindings = BindingManager()
 
   private val tonesPanel = TonesPanel.create(viewModel)
   private val harmonicsPanel = HarmonicsPanel.create(viewModel)
   private val reverbPanel = ReverbPanel.create(viewModel, reverbBindings)
-  private val modePanel = ModePanel.create(viewModel)
 
-  view.getChildren.addAll(tonesPanel, harmonicsPanel, reverbPanel, modePanel)
+  content.getChildren.addAll(tonesPanel, harmonicsPanel, reverbPanel)
+  HBox.setHgrow(harmonicsPanel, Priority.ALWAYS)
+
+  protected val view = VBox(content)
