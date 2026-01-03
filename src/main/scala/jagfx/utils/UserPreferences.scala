077559cc087f445751317e1f3f707306e27da09e
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-04 01:48:21 +0200

    move constant up

--- src/main/scala/jagfx/utils/UserPreferences.scala
+++ src/main/scala/jagfx/utils/UserPreferences.scala
@@ -3,14 +3,14 @@ package jagfx.utils
 import java.util.prefs.Preferences
 import javafx.beans.property.{BooleanProperty, SimpleBooleanProperty}
 
+private val KeyExport16Bit = "export_16_bit"
+
 /** Handles persistent user preferences using `java.util.prefs`. */
 object UserPreferences:
   private val prefs = Preferences.userNodeForPackage(getClass)
-  private val KeyExport16Bit = "export_16_bit"
 
   val export16Bit: BooleanProperty =
     new SimpleBooleanProperty(prefs.getBoolean(KeyExport16Bit, false))
-
   export16Bit.addListener((_, _, newVal) =>
     prefs.putBoolean(KeyExport16Bit, newVal)
   )
