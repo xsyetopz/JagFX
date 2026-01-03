a233ec8df954b3df76edb9cd0070e50ab29ab56b
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 07:29:05 +0200

    use `IViewModel` trait on these

--- src/main/scala/jagfx/ui/viewmodel/EnvelopeViewModel.scala
+++ src/main/scala/jagfx/ui/viewmodel/EnvelopeViewModel.scala
@@ -4,18 +4,17 @@ import javafx.beans.property._
 import jagfx.model._
 
 /** `ViewModel` for `Envelope` data. */
-class EnvelopeViewModel:
+class EnvelopeViewModel extends IViewModel:
   val form = new SimpleObjectProperty[WaveForm](WaveForm.Square)
   val start = new SimpleIntegerProperty(0)
   val end = new SimpleIntegerProperty(0)
   val notes = new SimpleIntegerProperty(0)
 
   private var segments: Vector[EnvelopeSegment] = Vector.empty
-  private var listeners: List[() => Unit] = Nil
 
   def getSegments: Vector[Int] = segments.map(_.peak)
   def getFullSegments: Vector[EnvelopeSegment] = segments
-  def isEmpty: Boolean = segments.isEmpty
+  def isEmpty: Boolean = segments.isEmpty && form.get == WaveForm.Off
 
   def load(env: Envelope): Unit =
     form.set(env.form)
@@ -34,8 +33,7 @@ class EnvelopeViewModel:
   def toModel(): Envelope =
     Envelope(form.get, start.get, end.get, segments)
 
-  def addChangeListener(listener: () => Unit): Unit =
-    listeners = listener :: listeners
-
-  private def notifyListeners(): Unit =
-    listeners.foreach(_())
+  override protected def registerPropertyListeners(cb: () => Unit): Unit =
+    form.addListener((_, _, _) => cb())
+    start.addListener((_, _, _) => cb())
+    end.addListener((_, _, _) => cb())
