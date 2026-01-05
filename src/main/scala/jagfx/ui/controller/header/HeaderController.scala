9625c2ad2b432d30eb5b6094ed26b54510bfe3cb
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-05 03:39:22 +0200

    use `_` prefix for private items

--- src/main/scala/jagfx/ui/controller/header/HeaderController.scala
+++ src/main/scala/jagfx/ui/controller/header/HeaderController.scala
@@ -12,66 +12,72 @@ import jagfx.Constants
 import jagfx.utils._
 import javafx.beans.property.SimpleBooleanProperty
 
+private val _LoopParamSize = 34
+
 /** Header controller containing transport, file, and settings controls. */
 class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
   import Constants._
 
-  private val audioPlayer = AudioPlayer(viewModel)
-  private val fileOps = FileOperations(viewModel, () => view.getScene.getWindow)
+  private val _audioPlayer = AudioPlayer(viewModel)
+  private val _fileOps =
+    FileOperations(viewModel, () => view.getScene.getWindow)
 
   /** Callback for playhead position updates. */
-  def onPlayheadUpdate: Double => Unit = audioPlayer.onPlayheadUpdate
+  def onPlayheadUpdate: Double => Unit = _audioPlayer.onPlayheadUpdate
   def onPlayheadUpdate_=(f: Double => Unit): Unit =
-    audioPlayer.onPlayheadUpdate = f
+    _audioPlayer.onPlayheadUpdate = f
 
   protected val view = GridPane()
   view.getStyleClass.add("header-grid")
   view.getStyleClass.add("header-root")
 
-  private val col1 = ColumnConstraints()
-  col1.setPercentWidth(20)
-  col1.setHalignment(HPos.LEFT)
+  private val _col1 = ColumnConstraints()
+  _col1.setPercentWidth(20)
+  _col1.setHalignment(HPos.LEFT)
 
-  private val col2 = ColumnConstraints()
-  col2.setPercentWidth(60)
-  col2.setHalignment(HPos.CENTER)
+  private val _col2 = ColumnConstraints()
+  _col2.setPercentWidth(60)
+  _col2.setHalignment(HPos.CENTER)
 
-  private val col3 = ColumnConstraints()
-  col3.setPercentWidth(20)
-  col3.setHalignment(HPos.RIGHT)
+  private val _col3 = ColumnConstraints()
+  _col3.setPercentWidth(20)
+  _col3.setHalignment(HPos.RIGHT)
 
-  view.getColumnConstraints.addAll(col1, col2, col3)
+  view.getColumnConstraints.addAll(_col1, _col2, _col3)
 
-  private val LoopParamSize = 34
+  private val _transportGroup = _createTransportGroup()
+  private val _tgtGroup = _createTargetGroup()
+  private val _lenPosGroup = _createLenPosGroup()
+  private val _loopGroup = _createLoopGroup()
+  private val _fileGroup = _createFileGroup()
+  private val _btn16Bit = _create16BitButton()
 
-  private val transportGroup = createTransportGroup()
-  private val tgtGroup = createTargetGroup()
-  private val lenPosGroup = createLenPosGroup()
-  private val loopGroup = createLoopGroup()
-  private val fileGroup = createFileGroup()
-  private val btn16 = create16BitButton()
+  private val _leftGroup = HBox(2)
+  _leftGroup.setAlignment(Pos.CENTER_LEFT)
+  _leftGroup.setPickOnBounds(false)
 
-  private val leftGroup = HBox(2)
-  leftGroup.setAlignment(Pos.CENTER_LEFT)
-  leftGroup.setPickOnBounds(false)
+  _leftGroup.getChildren.add(_transportGroup)
 
-  leftGroup.getChildren.add(transportGroup)
+  private val _centerGroup = HBox(2)
+  _centerGroup.setAlignment(Pos.CENTER)
+  _centerGroup.setPickOnBounds(false)
+  _centerGroup.getChildren.addAll(
+    _tgtGroup,
+    _lenPosGroup,
+    _loopGroup,
+    _btn16Bit
+  )
 
-  private val centerGroup = HBox(2)
-  centerGroup.setAlignment(Pos.CENTER)
-  centerGroup.setPickOnBounds(false)
-  centerGroup.getChildren.addAll(tgtGroup, lenPosGroup, loopGroup, btn16)
+  private val _rightGroup = HBox(2)
+  _rightGroup.setAlignment(Pos.CENTER_RIGHT)
+  _rightGroup.setPickOnBounds(false)
+  _rightGroup.getChildren.addAll(_fileGroup)
 
-  private val rightGroup = HBox(2)
-  rightGroup.setAlignment(Pos.CENTER_RIGHT)
-  rightGroup.setPickOnBounds(false)
-  rightGroup.getChildren.addAll(fileGroup)
+  view.add(_leftGroup, 0, 0)
+  view.add(_centerGroup, 1, 0)
+  view.add(_rightGroup, 2, 0)
 
-  view.add(leftGroup, 0, 0)
-  view.add(centerGroup, 1, 0)
-  view.add(rightGroup, 2, 0)
-
-  private def createTransportGroup(): HBox =
+  private def _createTransportGroup(): HBox =
     val btnPlay = JagButton()
     btnPlay.setGraphic(IconUtils.icon("mdi2p-play"))
     btnPlay.setTooltip(
@@ -87,8 +93,8 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
       new Tooltip("Toggle loop playback between start and end")
     )
 
-    btnPlay.setOnAction(_ => audioPlayer.play())
-    btnStop.setOnAction(_ => audioPlayer.stop())
+    btnPlay.setOnAction(_ => _audioPlayer.play())
+    btnStop.setOnAction(_ => _audioPlayer.stop())
     btnLoop.setOnAction(_ =>
       viewModel.loopEnabledProperty.set(!viewModel.isLoopEnabled)
     )
@@ -101,7 +107,7 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
     group.getStyleClass.add("h-grp")
     group
 
-  private def createTargetGroup(): HBox =
+  private def _createTargetGroup(): HBox =
     val group = HBox(2)
     group.getStyleClass.add("h-grp")
 
@@ -122,7 +128,7 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
     group.getChildren.addAll(Label("TGT"), btnOne, btnAll)
     group
 
-  private def createLenPosGroup(): HBox =
+  private def _createLenPosGroup(): HBox =
     val group = HBox(2)
     group.getStyleClass.add("h-grp")
     val durationField = JagNumericField(0, Int16.Range, 1200)
@@ -143,22 +149,22 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
     )
     group
 
-  private def createLoopGroup(): HBox =
+  private def _createLoopGroup(): HBox =
     val group = HBox(2)
     group.getStyleClass.add("h-grp")
 
     val fldL1 = JagNumericField(0, Int16.Range, 0)
-    fldL1.setPrefWidth(LoopParamSize)
+    fldL1.setPrefWidth(_LoopParamSize)
     fldL1.setTooltip(new Tooltip("Loop start position in samples"))
     fldL1.valueProperty.bindBidirectional(viewModel.loopStartProperty)
 
     val fldL2 = JagNumericField(0, Int16.Range, 0)
-    fldL2.setPrefWidth(LoopParamSize)
+    fldL2.setPrefWidth(_LoopParamSize)
     fldL2.setTooltip(new Tooltip("Loop end position in samples"))
     fldL2.valueProperty.bindBidirectional(viewModel.loopEndProperty)
 
     val fldCnt = JagNumericField(0, 100, 0)
-    fldCnt.setPrefWidth(LoopParamSize - 10)
+    fldCnt.setPrefWidth(_LoopParamSize - 10)
     fldCnt.setTooltip(new Tooltip("Number of loop repetitions (preview only)"))
     fldCnt.valueProperty.bindBidirectional(viewModel.loopCountProperty)
 
@@ -185,7 +191,7 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
     group.setDisable(true)
     group
 
-  private def createFileGroup(): HBox =
+  private def _createFileGroup(): HBox =
     val group = HBox(2)
     group.setStyle("-fx-border-color: transparent;")
     group.setAlignment(Pos.CENTER)
@@ -200,21 +206,21 @@ class HeaderController(viewModel: SynthViewModel) extends IController[GridPane]:
     btnExport.setGraphic(IconUtils.icon("mdi2e-export-variant"))
     btnExport.setTooltip(new Tooltip("Save as .synth or export to WAV"))
 
-    btnOpen.setOnAction(_ => fileOps.open())
-    btnSave.setOnAction(_ => fileOps.save())
-    btnExport.setOnAction(_ => fileOps.saveAs())
+    btnOpen.setOnAction(_ => _fileOps.open())
+    btnSave.setOnAction(_ => _fileOps.save())
+    btnExport.setOnAction(_ => _fileOps.saveAs())
 
     group.getChildren.addAll(btnOpen, btnSave, btnExport)
     group
 
-  private def create16BitButton(): JagButton =
+  private def _create16BitButton(): JagButton =
     val btn16 = JagButton("16-BIT")
     btn16.setTooltip(new Tooltip("8-bit (OFF) / 16-bit (ON)"))
     btn16.setOnAction(_ =>
-      UserPreferences.export16Bit.set(!UserPreferences.export16Bit.get)
+      UserPrefs.export16Bit.set(!UserPrefs.export16Bit.get)
     )
-    UserPreferences.export16Bit.addListener((_, _, enabled) =>
+    UserPrefs.export16Bit.addListener((_, _, enabled) =>
       btn16.setActive(enabled)
     )
-    btn16.setActive(UserPreferences.export16Bit.get)
+    btn16.setActive(UserPrefs.export16Bit.get)
     btn16
