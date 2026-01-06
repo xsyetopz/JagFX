f524132a4bde04d91a8ac3b60fd2ac05f99e43f1
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-06 04:14:23 +0200

    colour dimming helper; make constants `final`

--- src/main/scala/jagfx/utils/ColorUtils.scala
+++ src/main/scala/jagfx/utils/ColorUtils.scala
@@ -3,31 +3,34 @@ package jagfx.utils
 /** Colors synced with `variables.scss`. */
 object ColorUtils:
   // Backgrounds
-  val BgBlack: Int = 0xff000000
+  final val BgBlack: Int = 0xff000000
 
   // Borders
-  val BorderDim: Int = 0xff333333
+  final val BorderDim: Int = 0xff333333
 
   // Text
-  val White: Int = 0xfff0f0f0
+  final val White: Int = 0xfff0f0f0
 
   // Accent
-  val Graph: Int = 0xffee7733
+  final val Graph: Int = 0xffee7733
 
   // Filter
-  val FilterPole: Int = 0xff009e73
-  val FilterZero: Int = 0xffee7733
-  val FilterResponse: Int = 0xff33bb99
+  final val FilterPole: Int = 0xff009e73
+  final val FilterZero: Int = 0xffee7733
+  final val FilterResponse: Int = 0xff33bb99
 
   // Semantic
-  val Output: Int = 0xff009e73
-  val Gating: Int = 0xffcc79a7
+  final val Output: Int = 0xff009e73
+  final val Gating: Int = 0xffcc79a7
 
   // Grid
-  val GridLineFaint: Int = 0xff2a2a2a
-  val GridLineMinor: Int = 0xff1a1a1a
+  final val GridLineFaint: Int = 0xff2a2a2a
+  final val GridLineMinor: Int = 0xff1a1a1a
 
   // Editor control points
-  val PointNormal: Int = 0xffee7733
-  val PointHover: Int = 0xffffaa55
-  val PointSelected: Int = 0xffffffff
+  final val PointNormal: Int = 0xffee7733
+  final val PointHover: Int = 0xffffaa55
+  final val PointSelected: Int = 0xffffffff
+
+  /** Returns color with alpha channel dimmed to `~30%`. */
+  inline def dimColor(color: Int): Int = (color & 0x00ffffff) | 0x50000000
