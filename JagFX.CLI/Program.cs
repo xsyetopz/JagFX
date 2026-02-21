5b4a8e0a84d408eab267392f8c8ed6071ced11d8
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-02-21 02:36:06 +0200

    block syntax due to net8.0

--- JagFX.CLI/Program.cs
+++ JagFX.CLI/Program.cs
@@ -3,5 +3,7 @@ namespace JagFX.CLI;
 public static class Program
 {
     public static Task<int> Main(string[] args)
-        => JagFXCli.RunAsync(args);
+    {
+        return JagFXCli.RunAsync(args);
+    }
 }
