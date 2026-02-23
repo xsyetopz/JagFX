3acc86baf22ffd945435b297e3208d986ab2d960
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-02-23 03:51:54 +0200

    rename

--- src/JagFX.Domain/Models/LoopSegment.cs
+++ src/JagFX.Domain/Models/LoopSegment.cs
@@ -1,6 +1,6 @@
 namespace JagFX.Domain.Models;
 
-public record Loop(int BeginSample, int EndSample)
+public record LoopSegment(int BeginSample, int EndSample)
 {
     public bool IsActive => BeginSample < EndSample;
 }
